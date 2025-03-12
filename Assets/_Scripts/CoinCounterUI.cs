1bf2c8b0e5048a375ea1f5e12f7e0dd48ce4eaba
ved-b <90875244+ved-b@users.noreply.github.com>
2025-03-12 01:38:25 -0700

    Implemented animated Coin Counter

--- Assets/_Scripts/CoinCounterUI.cs
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -10,6 +10,7 @@ public class CoinCounterUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI toUpdate;
     [SerializeField] private Transform cointTextContainer;
     [SerializeField] private float duration;
+    [SerializeField] private Ease animationCurve;
 
     private float containerInitPosition;
     private float moveAmount;
@@ -34,5 +35,15 @@ public class CoinCounterUI : MonoBehaviour
         toUpdate.SetText($"{score}");
 
         cointTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration);
+        cointTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
+
+        StartCoroutine(ResetCointContainer(score));
+    }
+
+    private IEnumerator ResetCointContainer(int score){
+        yield return new WaitForSeconds(duration);
+        current.SetText($"{score}");
+        Vector3 localPosition = cointTextContainer.localPosition;
+        cointTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
     }
 }
